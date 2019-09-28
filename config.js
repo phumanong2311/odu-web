df96b6a5c76d61eb0966bcacb42ff73af72da76e
kage tris <phumanong@gmail.com>
2019-09-29 04:38:40 +0700

    update web oduvn

---
+++ config.js
@@ -0,0 +1,13 @@
+var config = {
+  server: {
+    protocol: 'http',
+    host: 'localhost',
+    port: '3100'
+  }
+}
+
+let { server } = config
+// config['server']['domain'] = `${server.protocol}://${server.host}:${server.port}`
+config['server']['domain'] = `http://api.oduvn.com`
+
+module.exports = config
